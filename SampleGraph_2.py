279024dd42a6c6cbc567874cc1ba534b2187fbbe
zengjin1974 <zengjin1974@icloud.com>
2019-10-04 21:40:53 +0800

    20191004

--- SampleGraph_2.py
+++ SampleGraph_2.py
@@ -2,12 +2,12 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
 
-plt.rcParams["font.sans-serif"] = 'SimHei'
+# plt.rcParams["font.sans-serif"] = 'SimHei'
 plt.rcParams["axes.unicode_minus"] = False
 
 # config InlineBackend.figure_format = 'svg'
 
-# fig = plt.figure()
+fig = plt.figure()
 # fig = plt.figure(figsize=(10, 8))
 
 # ax1 = fig.add_subplot(2, 2, 1)
@@ -15,14 +15,29 @@ plt.rcParams["axes.unicode_minus"] = False
 # ax3 = fig.add_subplot(2, 2, 3)
 # ax4 = fig.add_subplot(2, 2, 4)
 
-# ax1 = fig.add_subplot(1, 1, 1)
+ax1 = fig.add_subplot(1, 1, 1)
+plt.xlabel("Month")
+plt.ylabel("Register")
 
-fig, axes = plt.subplots(2, 2)
+plt.xticks(np.arange(9), ["Jan", "Feb", "Mar",
+                          "Apr", "May", "Jun",
+                          "Jul", "Aug", "Sep"])
 
-x = np.arange(6)
-y = np.arange(6)
+plt.yticks(np.arange(1000, 7000, 1000),
+           ["1000", "2000", "3000",
+            "4000", "5000", "6000"])
 
-axes[0, 0].plot(x, y)
-axes[1, 1].bar(x, y)
+x = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9])
+y = np.array([866, 2335, 5710, 6482, 6120, 1605, 3813, 4428, 4631])
+
+plt.plot(x, y)
+
+# fig, axes = plt.subplots(2, 2)
+#
+# x = np.arange(6)
+# y = np.arange(6)
+#
+# axes[0, 0].plot(x, y)
+# axes[1, 1].bar(x, y)
 
 plt.show()
